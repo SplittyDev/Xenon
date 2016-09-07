24f8fdb3abed6e73b7cae7d79ea214767ac67926
Splitty <splittydev@gmail.com>
2016-09-07 19:47:55 +0200

    Initial commit

---
+++ XenonTest/Test.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+using System;
+namespace XenonTest {
+    [TestFixture ()]
+    public class Test {
+        [Test ()]
+        public void TestCase () {
+        }
+    }
+}
+
